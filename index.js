d2837ec4537916a5b78169ef57b4b6c78feba4b1
nosson <48989814+nossonCotlar@users.noreply.github.com>
2020-12-31 00:43:21 -0600

    synchronous responses supported

--- index.js
+++ index.js
@@ -20,30 +20,46 @@ app.get('/:engine', genart);
 app.get('/:engine/:seed', genart);
 
 async function genart(req, res){
+    //determine if synchronous
+    let sync = req.query.sync;
     try{
         //find the seed
-        let theSeed;
+        let theSeed = req.query.seed || req.params.seed || randomstring.generate(10);
         let theEngine = req.query.engine || req.params.engine || process.env.DEFAULT_ENGINE;
-        if(req.query.seed) theSeed = req.query.seed;
-        else if(req.params.seed) theSeed = req.params.seed;
-        else theSeed = randomstring.generate();
 
+        //determine cloud file path
+        let cloudFilePath = `${theEngine}/${theSeed}.png`;
 
-        let out = bucket.file(`${theEngine}-${theSeed}.png`).createWriteStream({resumable: false});
+        //initialize write stream to cloud
+        let out = bucket.file(cloudFilePath).createWriteStream({resumable: false});
+        //generate the art canvas and make a png stream from it
         let stream = generate(theSeed, theEngine).createPNGStream();
+        //pipe png stream into cloud
         stream.pipe(out);
-        res.type('json').status(200).json({
+        
+        //define what to do when sending response
+        function sendStuff(){
+            res.status(200).json({
             seed: theSeed, 
             engine: theEngine,
-            link: encodeURI(`${process.env.GCLOUD_STORAGE_ENDPOINT}/${theEngine}-${theSeed}.png`)
-        });
+            link: encodeURI(`${process.env.GCLOUD_STORAGE_ENDPOINT}/${cloudFilePath}`)
+            });
+        }
+
+        //if the synchronous flag is set, only send once the art has been uploaded (slow)
+        if(sync == 'true'){
+            out.on('finish', sendStuff);
+        }
+        //if not just send the path and the art will be there eventually lol
+        else {
+            sendStuff();
+        }
+        
         console.log(`Generated on engine: ${theEngine}, using seed: ${theSeed}`);
     }
     catch(e){
-        res.type('json').status(500).json({ error: e});
+        res.status(500).json({ error: e});
     }
-    
-    
 }
 
 app.listen(process.env.PORT, () => console.log("We're gtg"));
